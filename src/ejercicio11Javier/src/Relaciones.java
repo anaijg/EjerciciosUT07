9b824b5cabb30495d5d187e3a33a3ef7377aa1d1
DanteSlay <danteslay16@gmail.com>
2023-04-12 18:40:35 +0200

    Ejercicio Javier

---
+++ src/ejercicio11Javier/src/Relaciones.java
@@ -0,0 +1,11 @@
+package ejercicio11Javier.src;
+
+import java.util.ArrayList;
+
+public interface Relaciones {
+    boolean esMayor(Object b);
+
+    boolean esMenor(Object b);
+
+    boolean esIgual(Object b);
+}
